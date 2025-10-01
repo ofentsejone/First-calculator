16e340c297aa921470f36958c92285179ec7dc9a
Ofentse Makuwa <ofentsejone@gmail.com>
2025-10-02 00:08:27 +0200

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,13 @@
+var number=document.getElementByName("nine");
+//getElementsByTagName('submit')
+
+
+document.write(number);
+
+/*function total(num1,num2)
+{
+
+  var total=num1+5;
+  document.write("  equals to "+total);
+}
+ total(3);*/
